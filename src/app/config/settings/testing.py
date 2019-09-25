85c4ed5135691e38ac0e1e1b0d53e006c17f6d47
Ramiro Gonzalez <rgonzalez@makingsense.com>
2019-09-25 12:43:40 -0300

    Rename config/env to settings

---
+++ src/app/config/settings/testing.py
@@ -0,0 +1,24 @@
+SECRET_KEY = 'fake-key'
+INSTALLED_APPS = [
+  "tests",
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+  'rest_framework',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'TEST': {
+          'DEPENDENCIES': []
+        }
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
